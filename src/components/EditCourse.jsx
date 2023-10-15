81d9459367f26e6b5e4ea75cd7b3d0db6b0cbe26
primelxz <xliu855@wisc.edu>
2023-10-14 21:05:07 -0500

    format validation

--- src/components/EditCourse.jsx
+++ src/components/EditCourse.jsx
@@ -1,22 +1,28 @@
 import { useState } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import "./EditCourse.css";
+import { useFormData } from '../utilities/useFormData.js';
+
+const validateUserData = (key, val) => {
+    switch (key) {
+        case 'title':
+            return /(^\w\w)/.test(val) ? '' : 'must be least two characters';
+        case 'meets':
+            return /^(MWF|TuTh) (\d{1,2}:\d{2}-\d{1,2}:\d{2})$|^$/.test(val)
+            ? '' : 'Please follow the format like MWF 9:00-9:50 or TuTh 14:00-15:20';;
+      default: return '';
+    }
+};
 
 const EditCourse = ({ courses }) => {
     const { courseId } = useParams();
     const course = courses[courseId];
     const navigate = useNavigate();
 
-    const [title, setTitle] = useState(course.title);
-    const [meets, setMeets] = useState(course.meets);
-
-    const handleTitleChange = (e) => {
-        setTitle(e.target.value);
-    };
-
-    const handleMeetsChange = (e) => {
-        setMeets(e.target.value);
-    };
+    const [state, change] = useFormData(validateUserData, {
+        title: course.title,
+        meets: course.meets
+    });
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -31,16 +37,21 @@ const EditCourse = ({ courses }) => {
             <form onSubmit={handleSubmit}>
                 <div className="form-group">
                     <label htmlFor="course-title">Course Title</label>
-                    <input type="text" id="course-title" value={title} onChange={handleTitleChange} />
+                    <input type="text" id="title" value={state.values.title} onChange={change} />
+                    {state.errors && state.errors.title &&
+                    <div className="error-message">{state.errors.title}</div>}
                 </div>
 
                 <div className="form-group">
                     <label htmlFor="course-meets">Course Meets</label>
-                    <input type="text" id="course-meets" value={meets} onChange={handleMeetsChange} />
+                    <input type="text" id="meets" value={state.values.meets} onChange={change} />
+                    {state.errors && state.errors.meets &&
+                    <div className="error-message">{state.errors.meets}</div>}
                 </div>
                 <button onClick={handleCancel}>Cancel</button>
             </form>
         </div>
-)};
+    )
+};
 
 export default EditCourse;
